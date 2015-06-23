57a81d44a56076e8a77a24201b4abcfeed6b149d
TarCode <TarCode@github.com>
2015-06-23 08:38:25 +0200

    check board size, initialized margins, defined color variables

--- memoryBlocks.py
+++ memoryBlocks.py
@@ -13,3 +13,19 @@ BOXSIZE = 100 		# box height and width in pixels
 GAPSIZE = 25 		# gap size between boxes in pixels
 BOARDWIDTH = 4 		# number of columns of icons
 BOARDHEIGHT = 4 	# number of rows of icons
+
+assert (BOARDWIDTH * BOARDHEIGHT) % 2 == 0, 'Board needs to have an even number of boxes for pairs of matches.'
+XMARGIN = int((WINDOWWIDTH - (BOARDWIDTH * (BOXSIZE + GAPSIZE))) / 2)
+YMARGIN = int((WINDOWHEIGHT - (BOARDHEIGHT * (BOXSIZE + GAPSIZE))) / 2)
+
+           # R    G    B
+GRAY     = (100, 100,  50)
+BLACK    = ( 0,    0,   0)
+WHITE    = (200, 250, 250)
+RED      = (200,   0,   0)
+GREEN    = (  0, 200,   0)
+BLUE     = (  0,   0, 200)
+YELLOW   = (255, 200,   0)
+ORANGE   = (200, 120,   0)
+PURPLE   = (250,   0, 200)
+CYAN     = (  0, 250, 200)
