7301032c3c91a848850de437a525705859cda961
TarCode <TarCode@github.com>
2015-06-23 08:45:05 +0200

    get box at pixel function

--- memoryBlocks.py
+++ memoryBlocks.py
@@ -176,5 +176,15 @@ def leftTopCoordsOfBox(boxx, boxy):
     top = boxy * (BOXSIZE + GAPSIZE) + YMARGIN
     return (left, top)
 
+#################################################################################################################
+
+def getBoxAtPixel(x, y):
+    for boxx in range(BOARDWIDTH):
+        for boxy in range(BOARDHEIGHT):
+            left, top = leftTopCoordsOfBox(boxx, boxy)
+            boxRect = pygame.Rect(left, top, BOXSIZE, BOXSIZE)
+            if boxRect.collidepoint(x, y):
+                return (boxx, boxy)
+    return (None, None)
 
 
